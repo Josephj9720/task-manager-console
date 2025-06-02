4efb85dea4b15b54bd41bf1cd7335e3c1cf28feb
Joseph9720 <Josephj9720@gmail.com>
2025-06-02 19:05:22 -0400

    added LoggerUtil to use in every class

--- src/main/java/dev/jordanjoseph/taskmanager/Main.java
+++ src/main/java/dev/jordanjoseph/taskmanager/Main.java
@@ -13,49 +13,49 @@ public class Main {
     public static void main(String[] args) {
 
         TaskServiceImpl taskServiceImpl = new TaskServiceImpl();
-//        Scanner scanner = new Scanner(System.in);
-//
-//        System.out.println("=== List All Tasks ===");
-//        for(Task task : taskServiceImpl.listTasks()) {
-//            System.out.println(task);
-//        }
-//        System.out.println("=====================");
-//
-//        System.out.println("=== Create a Task ===");
-//        System.out.print("Title: ");
-//        String title = scanner.nextLine();
-//
-//        System.out.print("Description: ");
-//        String description = scanner.nextLine();
-//
-//        System.out.print("Due date (YYYY-MM-DD): ");
-//        LocalDate dueDate  = LocalDate.parse(scanner.nextLine());
-//
-//        taskServiceImpl.createTask(title, description, dueDate);
-//
-//        System.out.println("=====================");
-//
-//        System.out.println("=== Update a Task ===");
-//        System.out.print("Id: ");
-//        Long updateId = scanner.nextLong();
-//        taskServiceImpl.markCompleted(updateId);
-//
-//        System.out.println("=== List All Tasks After Changes ===");
-//        for(Task task : taskServiceImpl.listTasks()) {
-//            System.out.println(task);
-//        }
-//        System.out.println("=====================");
-//
-//        System.out.println("=== Delete a Task ===");
-//        System.out.print("Id: ");
-//        Long deleteId = scanner.nextLong();
-//        taskServiceImpl.deleteTask(deleteId);
-//
-//        System.out.println("=== List All Tasks After Changes ===");
-//        for(Task task : taskServiceImpl.listTasks()) {
-//            System.out.println(task);
-//        }
-//        System.out.println("=====================");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("=== List All Tasks ===");
+        for(Task task : taskServiceImpl.listTasks()) {
+            System.out.println(task);
+        }
+        System.out.println("=====================");
+
+        System.out.println("=== Create a Task ===");
+        System.out.print("Title: ");
+        String title = scanner.nextLine();
+
+        System.out.print("Description: ");
+        String description = scanner.nextLine();
+
+        System.out.print("Due date (YYYY-MM-DD): ");
+        LocalDate dueDate  = LocalDate.parse(scanner.nextLine());
+
+        taskServiceImpl.createTask(title, description, dueDate);
+
+        System.out.println("=====================");
+
+        System.out.println("=== Update a Task ===");
+        System.out.print("Id: ");
+        Long updateId = scanner.nextLong();
+        taskServiceImpl.markCompleted(updateId);
+
+        System.out.println("=== List All Tasks After Changes ===");
+        for(Task task : taskServiceImpl.listTasks()) {
+            System.out.println(task);
+        }
+        System.out.println("=====================");
+
+        System.out.println("=== Delete a Task ===");
+        System.out.print("Id: ");
+        Long deleteId = scanner.nextLong();
+        taskServiceImpl.deleteTask(deleteId);
+
+        System.out.println("=== List All Tasks After Changes ===");
+        for(Task task : taskServiceImpl.listTasks()) {
+            System.out.println(task);
+        }
+        System.out.println("=====================");
 
         System.out.println("=== Seed DB - Default ===");
         TaskSeeder taskSeeder = new TaskSeeder();
---
+++ src/main/java/dev/jordanjoseph/taskmanager/util/LoggerUtil.java
@@ -0,0 +1,9 @@
+package dev.jordanjoseph.taskmanager.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+//for a bigger application, use per-class logger
+public class LoggerUtil {
+    public static final Logger logger = LoggerFactory.getLogger(LoggerUtil.class);
+}
