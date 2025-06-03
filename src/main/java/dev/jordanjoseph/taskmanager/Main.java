8c0a007de74582b626af476c280693abba5151a2
Joseph9720 <Josephj9720@gmail.com>
2025-06-02 23:34:36 -0400

    created functional terminal menu for application

--- src/main/java/dev/jordanjoseph/taskmanager/Main.java
+++ src/main/java/dev/jordanjoseph/taskmanager/Main.java
@@ -14,70 +14,111 @@ public class Main {
 
         TaskServiceImpl taskServiceImpl = new TaskServiceImpl();
         Scanner scanner = new Scanner(System.in);
+        int choice = -1;
 
-        System.out.println("=== List All Tasks ===");
-        for(Task task : taskServiceImpl.listTasks()) {
-            System.out.println(task);
+        System.out.print("==========================================");
+        System.out.print(" Task Manager Application ");
+        System.out.println("==========================================");
+
+        while(choice != 0) {
+            System.out.print("=================================================");
+            System.out.print(" List Tasks ");
+            System.out.println("=================================================");
+
+            for(Task task : taskServiceImpl.listTasks()) {
+                System.out.println(task);
+            }
+
+            while(choice < 0 || choice > 3) {
+
+                System.out.print("=================================================");
+                System.out.print(" Operations ");
+                System.out.println("=================================================");
+
+                System.out.println("[1] Create a Task");
+                System.out.println("[2] Mark a Task as Completed");
+                System.out.println("[3] Delete a Task");
+                System.out.println("\n[0] Exit");
+
+                System.out.print("=================================================");
+                System.out.print("============");
+                System.out.println("=================================================");
+
+                try {
+                    choice = Integer.parseInt(scanner.nextLine());
+                } catch (Exception e) {
+                    System.out.println("Please input a whole number!");
+                    System.out.println("Please use the specified options.");
+                }
+            }
+
+            switch(choice) {
+                case 1:
+                    System.out.print("================================================");
+                    System.out.print(" Create  Task ");
+                    System.out.println("================================================");
+                    try {
+                        System.out.print("Task Title: ");
+                        String title = scanner.nextLine();
+                        System.out.print("Task Description: ");
+                        String description = scanner.nextLine();
+                        System.out.print("Due Date - YYYY-MM-DD: ");
+                        LocalDate dueDate = LocalDate.parse(scanner.nextLine());
+                        taskServiceImpl.createTask(title, description, dueDate);
+
+                    } catch (Exception e) {
+                        System.out.println("An error occurred during Task Creation. Try again.");
+                    }
+
+                    choice = -1;
+                    break;
+
+                case 2:
+                    System.out.print("==========================================");
+                    System.out.print(" Mark a Task as Completed ");
+                    System.out.println("==========================================");
+                    System.out.print("Task Id: ");
+                    long toCompletedId;
+                    while(true) {
+                        try {
+                            toCompletedId = Long.parseLong(scanner.nextLine());
+                            break;
+                        } catch (Exception e) {
+                            System.out.println("Please input a whole number!");
+                        }
+                    }
+                    taskServiceImpl.markCompleted(toCompletedId);
+                    choice = -1;
+                    break;
+
+                case 3:
+                    System.out.print("=======================================================");
+                    System.out.print(" Delete  Task ");
+                    System.out.println("=======================================================");
+                    System.out.print("Task Id: ");
+                    long toDeleteId;
+                    while(true) {
+                        try {
+                            toDeleteId = Long.parseLong(scanner.nextLine());
+                            break;
+                        } catch (Exception e) {
+                            System.out.println("Please input a whole number!");
+                        }
+                    }
+                    taskServiceImpl.deleteTask(toDeleteId);
+                    choice = -1;
+                    break;
+
+                default:
+                    break;
+            }
+
         }
-        System.out.println("=====================");
-
-        System.out.println("=== Create a Task ===");
-        System.out.print("Title: ");
-        String title = scanner.nextLine();
-
-        System.out.print("Description: ");
-        String description = scanner.nextLine();
-
-        System.out.print("Due date (YYYY-MM-DD): ");
-        LocalDate dueDate  = LocalDate.parse(scanner.nextLine());
-
-        taskServiceImpl.createTask(title, description, dueDate);
-
-        System.out.println("=====================");
-
-        System.out.println("=== Update a Task ===");
-        System.out.print("Id: ");
-        Long updateId = scanner.nextLong();
-        taskServiceImpl.markCompleted(updateId);
-
-        System.out.println("=== List All Tasks After Changes ===");
-        for(Task task : taskServiceImpl.listTasks()) {
-            System.out.println(task);
-        }
-        System.out.println("=====================");
-
-        System.out.println("=== Delete a Task ===");
-        System.out.print("Id: ");
-        Long deleteId = scanner.nextLong();
-        taskServiceImpl.deleteTask(deleteId);
-
-        System.out.println("=== List All Tasks After Changes ===");
-        for(Task task : taskServiceImpl.listTasks()) {
-            System.out.println(task);
-        }
-        System.out.println("=====================");
-
-        System.out.println("=== Seed DB - Default ===");
-        TaskSeeder taskSeeder = new TaskSeeder();
-        taskSeeder.run();
-
-        System.out.println("=== List All Tasks After Changes ===");
-        for(Task task : taskServiceImpl.listTasks()) {
-            System.out.println(task);
-        }
-        System.out.println("=====================");
-
-        System.out.println("=== Seed DB - Parameter ===");
-        taskSeeder.run(20);
-
-        System.out.println("=== List All Tasks After Changes ===");
-        for(Task task : taskServiceImpl.listTasks()) {
-            System.out.println(task);
-        }
-        System.out.println("=====================");
-
-
-
 
     }
+
+    public void selectOperation(int choice) {
+
+    }
+
 }
