26d43e5f3cc899f832a54db72f3324c50bc6a3f1
Joseph9720 <Josephj9720@gmail.com>
2025-06-02 20:49:39 -0400

    added dependencies for H2-In-Memory-Database and JUnit 5 for testing

---
+++ src/test/java/dev/jordanjoseph/TaskTest.java
@@ -0,0 +1,50 @@
+package dev.jordanjoseph;
+
+import dev.jordanjoseph.taskmanager.model.Task;
+import dev.jordanjoseph.taskmanager.service.TaskServiceImpl;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class TaskTest {
+    private static TaskServiceImpl taskService;
+
+    @BeforeAll
+    static void setUp() {
+        taskService = new TaskServiceImpl();
+    }
+
+    @Test
+    void testCreateTest() {
+        for(int i = 0; i < 5; i++) {
+            taskService.createTask("Test Task " + i, "testing", LocalDate.now());
+        }
+    }
+
+    @Test
+    void testMarkCompleted() {
+        taskService.markCompleted(2L);
+    }
+
+    @Test
+    void testDeleteTask() {
+        taskService.deleteTask(1L);
+    }
+
+    @Test
+    void testListTasks() {
+        List<Task> tasks = taskService.listTasks();
+        for(Task task : tasks) {
+            assertNotNull(task);
+        }
+    }
+
+
+
+
+}
+
